37c4e1f611d505643e8092ef9bf7540d9022b2a2
LeQuocBao <quocbao10a12@gmail.com>
2021-11-07 15:42:45 +0700

    Thêm code từ dòng 21 - Hoàn thiện file main.js

--- public/js/main.js
+++ public/js/main.js
@@ -18,3 +18,72 @@ socket.on('roomUsers', ({ room, users }) => {
     outputRoomName(room);
     outputUsers(users);
 });
+
+// Tin nhắn từ server
+socket.on('message', (message) => {
+    console.log(message);
+    outputMessage(message);
+
+    // Cuộn xuống
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Gửi tin nhắn
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Nhận nội dung tin nhắn
+    let msg = e.target.elements.msg.value;
+
+    msg = msg.trim();
+
+    if (!msg) {
+        return false;
+    }
+
+    // Gửi tin nhắn đến máy chủ
+    socket.emit('chatMessage', msg);
+
+    // Xóa đoạn chữ vừa ghi - input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+// Xuất tin nhắn ra DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Thêm tên phòng vào DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Thêm những người dùng vào DOM
+function outputUsers(users) {
+    userList.innerHTML = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
+}
+
+//Nhắc người dùng trước khi rời khỏi phòng trò chuyện
+document.getElementById('leave-btn').addEventListener('click', () => {
+    const leaveRoom = confirm('Bạn có chắc chắn muốn rời khỏi phòng trò chuyện không?');
+    if (leaveRoom) {
+        window.location = '../index.html';
+    } else {}
+});
