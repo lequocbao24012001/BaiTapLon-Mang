e7975fe582ef5c519d377485b2ef635414a7ab1c
ngocnhat2k1 <76199236+ngocnhat2k1@users.noreply.github.com>
2021-11-13 23:29:52 +0700

    sửa lỗi giờ

--- utils/messages.js
+++ utils/messages.js
@@ -5,7 +5,7 @@ function formatMessage(username, text) {
     return {
         username,
         text,
-        time: moment().utc(7).format('h:mm a')
+        time: moment().utc(+07).format('h:mm a')
     };
 }
 
